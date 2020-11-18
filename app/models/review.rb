a8f5c5d9c562a30cf623081c6b1193e9453987aa
Claire Guillou <guillou.clr@gmail.com>
2020-11-18 16:53:51 +0100

    done

---
+++ app/models/review.rb
@@ -0,0 +1,8 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :content, presence: true
+  validates :rating, numericality: { only_integer: true }, presence: true
+
+  RATINGS = [0, 1, 2, 3, 4, 5]
+  validates :rating, inclusion: {in: RATINGS}
+end
